2122fc54561796074a13c764e609d8a4b3dc95fa
haroonrashidzadran <haroonrashidzadran@gmail.com>
2026-01-29 13:25:36 +0430

    Slightly extend loading screen duration

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
     }
     
     window.addEventListener('keydown', handleKeyPress)
-    setTimeout(() => setIsLoading(false), 1000)
+    setTimeout(() => setIsLoading(false), 1100)
     return () => window.removeEventListener('keydown', handleKeyPress)
   }, [])
 
